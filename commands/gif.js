e5fcf41ec29805e1d02896446ca0d686d8442593
Crimson <60007253+CrimsonDynamic@users.noreply.github.com>
2021-01-17 20:57:10 +0100

    adjusted again

--- commands/gif.js
+++ commands/gif.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 module.exports = async function (msg, args){
 keywords = args;
-let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&limit=30`
+let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&limit=12`
 let respone = await fetch(url);
 let json = await respone.json();
 console.log(json);
