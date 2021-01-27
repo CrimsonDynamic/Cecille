569002d0741ac505c7b59fcf67125d91a27708fd
CrimsonDynamic <LeonBarratt@protonmail.com>
2021-01-27 11:42:10 +0100

    fixed abuse of aaaaa command

--- commands/aaaaa.js
+++ commands/aaaaa.js
@@ -2,6 +2,7 @@ module.exports = function(msg,args){
     if(number = NaN) return;
     var i;
     number = args;
+    if(number>50) return; // Failsafe because abuse
     for( i=0; i< number;i++){
         msg.channel.send(`aaaaaaaaaaaaaaaaaaaaaaaaaa`);
     }
